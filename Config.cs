ed40b475a88fe37ff444dedd83c663b287f03171
Rui Li <woshilirui@hotmail.com>
2020-08-19 11:38:24 +1200

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,52 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Test;
+
+namespace coreEmptyIs4
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> Apis =>
+            new List<ApiResource>
+            {
+                new ApiResource("api1", "My API"),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                },
+            };
+
+        public static IEnumerable<IdentityResource> Ids =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static List<TestUser> Users =>     
+            new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "test1",
+                    Password = "test1"
+                }
+            };
+        
+    }
+}
