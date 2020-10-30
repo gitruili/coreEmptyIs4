3d2e0edfddc639234b9fb30565013b514818aa11
Rui Li <woshilirui@hotmail.com>
2020-10-30 21:51:27 +1300

    new model

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coreEmptyIs4.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        //can be extended later
+    }
+}
